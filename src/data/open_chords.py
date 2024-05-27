6818d0dd2d96ad1e5f10645abdb3e26a55dc17be
Alexandre D'Hooge <alexandre@petrucci>
2024-05-27 16:56:17 +0200

    restart from scratch

--- src/data/open_chords.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-def shape_is_open_chord(shape: str) -> bool:
-    return '0' in shape
-
-def manyhot_is_open_chord(manyhot: torch.Tensor) -> bool:
-    return (manyhot[:, 0] == 1).any()
--- src/exceptions.py
+++
@@ -1,2 +0,0 @@
-class RootError(Exception):
-    pass
